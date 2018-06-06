98fce0afc0e0c7deebffa8328a492732a231a573
dcartist <pbannerman13@gmail.com>
2018-06-06 21:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function removeFromCart(item) {
   var cartmatch = cart
   for (var i = cart.length - 1; i >= 0; i--){
       if(cart[i].itemName == item){
-        cart = cart.slice(i,1)
+        cart.slice(i,1)
       }
 
     // if (typeof cart[i].itemName != undefined){
